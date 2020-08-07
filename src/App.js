aa7e8246a0c88bf0c01a358164f7282143806db4
Roy <jackman.roy@gmail.com>
2020-08-07 00:53:05 -0400

    New react site

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './svg/alert.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This page is <strong>under construction!</strong> Please come back later, thank you!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/royJackman"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
