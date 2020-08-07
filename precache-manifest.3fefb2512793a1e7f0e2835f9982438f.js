42d401670766a9e2cebff351da5a301fabd2d5c2
Roy <jackman.roy@gmail.com>
2020-08-07 01:01:05 -0400

    Updates

---
+++ precache-manifest.3fefb2512793a1e7f0e2835f9982438f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de040045f0e90b57980d23a3a01908da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2afe3630703be37e2fb4",
+    "url": "/static/css/main.87672e21.chunk.css"
+  },
+  {
+    "revision": "d76d86cf642dd1a7f268",
+    "url": "/static/js/2.b48c7236.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b48c7236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2afe3630703be37e2fb4",
+    "url": "/static/js/main.daf0848f.chunk.js"
+  },
+  {
+    "revision": "36077e94b64b2e145941",
+    "url": "/static/js/runtime-main.c564bc2f.js"
+  },
+  {
+    "revision": "f6b20ced83abea7408f839c6596f16fb",
+    "url": "/static/media/alert.f6b20ced.svg"
+  }
+]);
