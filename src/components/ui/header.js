43d9d83afd482050b7fe51a436ed31318c99ae9d
Roy <jackman.roy@gmail.com>
2020-08-09 04:29:59 -0400

    Added 5 color scheme and navigation buttons

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Button} from 'react-bootstrap';
 import './ui.css';
 import logo from '../../svg/crow.svg';
 
 const Header = () => {
     return (
         <div>
-            <Container fluid className="primary-color white-text">
+            <Container fluid className="primary-color">
                 <Row>
                     <Col sm={1}>
-                        <img src={logo} alt="logo"/>
+                        <a href="/"><img src={logo} alt="logo"/></a>
                     </Col>
                     <Col />
-                    <Col md="auto">
-                        <h3 className="title">Resume</h3>
+                    <Col md="auto" className="center-text">
+                        <Button block href="/" variant="outline-dark">Home</Button>
+                    </Col>
+                    <Col md="auto" className="center-text">
+                        <Button block href="/resume" variant="outline-dark">Resume</Button>
                     </Col>
                 </Row>
             </Container>
