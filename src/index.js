06468ac53aa8cf94fef7df0fcf69885049211d71
royJackman <jackman.roy@gmail.com>
2020-08-08 21:13:38 -0400

    Added footer and base colors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
+import Footer from './components/footer/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const router = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path='/' component={App} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
