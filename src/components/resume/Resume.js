65cac826424f2b5163e8ce0de1af29c65a6a3b93
royJackman <jackman.roy@gmail.com>
2020-08-12 01:33:14 -0400

    Added work and educational history

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -7,9 +7,49 @@ function Resume() {
         <Container>
             <Row>
                 <Col />
-                <Col md={11}>
+                <Col md={13}>
                     <ul>
                         {new resumeItem(
+                            "mscs",
+                            "University of Massachusetts at Amherst",
+                            "Masters of Science in Computer Science focusing on Machine Learning and Artificial Intelligence",
+                            [
+                                "Courses include artificial intelligence, machine learning, reinforcement learning, natural language processing, neural learning, biological computing, bioinformatics, penetration testing, distributed systems, programming languages, digital forensics, and internet law.",
+                                "Performed research on recurrent learning models and lightshift computation.",
+                                "GPA: 3.6"
+                            ],
+                            new Date(2018, 4),
+                            new Date(2020, 7),
+                            <Image 
+                                src="https://raw.githubusercontent.com/detain/svg-logos/master/svg/umass.svg" 
+                                alt="UMass logo"
+                                style={{
+                                    backgroundColor: "white",
+                                    borderRadius: "5px",
+                                    padding: "5px"
+                                }} fluid/>,
+                            [
+                                "https://www.vectorlogo.zone/logos/python/python-icon.svg",
+                                "https://www.vectorlogo.zone/logos/numpy/numpy-icon.svg",
+                                "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg",
+                                "https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg",
+                                "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg",
+                                "https://raw.githubusercontent.com/abranhe/programming-languages-logos/master/src/cpp/cpp.svg",
+                                "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_csharp2.svg",
+                                "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_matlab.svg",
+                                "https://www.vectorlogo.zone/logos/r-project/r-project-icon.svg",
+                                "https://www.vectorlogo.zone/logos/rust-lang/rust-lang-icon.svg",
+                                "https://raw.githubusercontent.com/detain/svg-logos/master/svg/xamarin.svg",
+                                "https://www.vectorlogo.zone/logos/dotnet/dotnet-icon.svg",
+                                "https://png2.cleanpng.com/sh/0c977f414eb61d26d12880a08a4df670/L0KzQYm3VMA4N6dvj5H0aYP2gLBuTftidJoyhNt3dYiwcrLqiCRzaZRwReJubnX3grL7if9vNaVqiAY2b3bpdbB6igZmNaRqRd5yboX7PYbog8hjPWVoUdVqY3G7PomAWck6OmY5Sac7M0GzQ4G8UsY1OWkziNDw/kisspng-kali-linux-backtrack-penetration-test-offensive-se-linux-5ac8b54c9caca8.8799925415231030526418.png",
+                                "https://raw.githubusercontent.com/maxogden/hexbin/gh-pages/vector/wireshark.svg",
+                                "https://www.vectorlogo.zone/logos/amazon_aws/amazon_aws-icon.svg",
+                                "https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg",
+                            ],
+                            "#f8d197"
+                        )}
+                        {new resumeItem(
+                            "aaq",
                             "Applause App Quality", 
                             "Full Stack Software Engineer",
                             [
@@ -19,7 +59,14 @@ function Resume() {
                             ], 
                             new Date(2019, 5),
                             new Date(2020, 3), 
-                            <Image src="http://seekvectorlogo.com/wp-content/uploads/2019/01/applause-vector-logo.png" alt="Applause logo" fluid/>,
+                            <Image 
+                                src="http://seekvectorlogo.com/wp-content/uploads/2019/01/applause-vector-logo.png" 
+                                alt="Applause logo" 
+                                style={{
+                                    backgroundColor: "white",
+                                    borderRadius: "5px",
+                                    padding: "5px"
+                                }} fluid/>,
                             [
                                 "https://www.vectorlogo.zone/logos/java/java-icon.svg",
                                 "https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg",
@@ -29,9 +76,11 @@ function Resume() {
                                 "https://www.vectorlogo.zone/logos/ruby-lang/ruby-lang-icon.svg",
                                 "https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg",
                                 "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_maven.svg"
-                            ]
+                            ],
+                            '#f8e297'
                         )}
                         {new resumeItem(
+                            "binds",
                             "Biologically Inspired Neural and Dynamical Systems Laboratory",
                             "Research Assistant",
                             [
@@ -41,7 +90,14 @@ function Resume() {
                             ],
                             new Date(2016, 11),
                             new Date(2019, 5),
-                            <Image src="https://groups.cs.umass.edu/binds/wp-content/uploads/sites/21/2019/05/binds.png" alt="BINDS logo" fluid/>,
+                            <Image 
+                                src="https://groups.cs.umass.edu/binds/wp-content/uploads/sites/21/2019/05/binds.png" 
+                                alt="BINDS logo" 
+                                style={{
+                                    backgroundColor: "black",
+                                    borderRadius: "5px",
+                                    padding: "5px"
+                                }} fluid/>,
                             [
                                 "https://www.vectorlogo.zone/logos/python/python-icon.svg",
                                 "https://www.vectorlogo.zone/logos/r-project/r-project-icon.svg",
@@ -50,10 +106,97 @@ function Resume() {
                                 "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg",
                                 "https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg",
                                 "https://www.vectorlogo.zone/logos/amazon_aws/amazon_aws-icon.svg",
-                                "https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg"
-                            ]
+                                "https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg"
+                            ],
+                            '#f8e297'
                         )}
                         {new resumeItem(
+                            "bscs",
+                            "University of Massachusetts at Amherst",
+                            "Bachelors of Science in Computer Science focusing on Machine Learning and Artificial Intelligence",
+                            [
+                                "Courses include artificial intelligence, machine learning, robotics, natural language processing, mobile computing, bioinformatics, advanced algorithm design",
+                                "Performed research on reservoir computing and homgenous learning architectures",
+                                "Undergraduate thesis: Sine Circle Mappings in Machine Learning",
+                                "GPA: 3.5"
+                            ],
+                            new Date(2015, 8),
+                            new Date(2018, 4),
+                            <Image 
+                                src="https://raw.githubusercontent.com/detain/svg-logos/master/svg/umass.svg" 
+                                alt="UMass logo"
+                                style={{
+                                    backgroundColor: "white",
+                                    borderRadius: "5px",
+                                    padding: "5px"
+                                }} fluid/>,
+                            [
+                                "https://www.vectorlogo.zone/logos/python/python-icon.svg",
+                                "https://www.vectorlogo.zone/logos/numpy/numpy-icon.svg",
+                                "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg",
+                                "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg",
+                                "https://raw.githubusercontent.com/abranhe/programming-languages-logos/master/src/cpp/cpp.svg",
+                                "https://www.vectorlogo.zone/logos/java/java-icon.svg",
+                                "https://www.vectorlogo.zone/logos/javascript/javascript-icon.svg",
+                                "https://www.vectorlogo.zone/logos/scala-lang/scala-lang-icon.svg",
+                                "https://www.vectorlogo.zone/logos/angular/angular-icon.svg",
+                                "https://raw.githubusercontent.com/detain/svg-logos/master/svg/nativescript.svg",
+                                "https://raw.githubusercontent.com/detain/svg-logos/master/svg/vue-9.svg",
+                                "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_matlab.svg",
+                                "https://www.vectorlogo.zone/logos/r-project/r-project-icon.svg",
+                                "https://www.vectorlogo.zone/logos/amazon_aws/amazon_aws-icon.svg",
+                            ],
+                            "#f8d197"
+                        )}
+                        {new resumeItem(
+                            "bsm",
+                            "University of Massachusetts at Amherst",
+                            "Bachelors of Science in Mathematics focusing on Applied Mathematics",
+                            [
+                                "Courses include mathematical modeling, computation mathematics, advanced calculus, real anlysis, partial differential equations",
+                                "GPA: 3.6"
+                            ],
+                            new Date(2015, 8),
+                            new Date(2018, 4),
+                            <Image 
+                                src="https://raw.githubusercontent.com/detain/svg-logos/master/svg/umass.svg" 
+                                alt="UMass logo"
+                                style={{
+                                    backgroundColor: "white",
+                                    borderRadius: "5px",
+                                    padding: "5px"
+                                }} fluid/>,
+                            [
+                                
+                                "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_matlab.svg",
+                                "https://www.vectorlogo.zone/logos/r-project/r-project-icon.svg",
+                                "https://raw.githubusercontent.com/detain/svg-logos/master/svg/mathematica-1.svg",
+
+                            ],
+                            "#f8d197"
+                        )}
+                        {new resumeItem(
+                            "ml",
+                            "University of Massachusetts at Amherst",
+                            "Minor of Linguistics",
+                            [
+                                "Courses include syntax, early language development, phonetics, natural language processing"
+                            ],
+                            new Date(2015, 8),
+                            new Date(2018, 4),
+                            <Image 
+                                src="https://raw.githubusercontent.com/detain/svg-logos/master/svg/umass.svg" 
+                                alt="UMass logo"
+                                style={{
+                                    backgroundColor: "white",
+                                    borderRadius: "5px",
+                                    padding: "5px"
+                                }} fluid/>,
+                            [],
+                            "#f8d197"
+                        )}
+                        {new resumeItem(
+                            "tb",
                             "Twist Bioscience",
                             "Software Development Intern",
                             [
@@ -63,7 +206,14 @@ function Resume() {
                             ],
                             new Date(2018, 4),
                             new Date(2018, 7),
-                            <Image src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Twist_Bioscience_Official_Logo.png" alt="Twist logo" fluid/>,
+                            <Image 
+                                src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Twist_Bioscience_Official_Logo.png" 
+                                alt="Twist logo"
+                                style={{
+                                    backgroundColor: "white",
+                                    borderRadius: "5px",
+                                    padding: "5px"
+                                }} fluid/>,
                             [
                                 "https://www.vectorlogo.zone/logos/javascript/javascript-icon.svg",
                                 "https://upload.wikimedia.org/wikipedia/commons/c/c3/Ruby_on_Rails_logo.svg",
@@ -72,9 +222,11 @@ function Resume() {
                                 "https://www.vectorlogo.zone/logos/djangoproject/djangoproject-icon.svg",
                                 "https://raw.githubusercontent.com/gilbarbara/logos/master/logos/swagger.svg",
                                 "https://www.vectorlogo.zone/logos/mailchimp/mailchimp-icon.svg"
-                            ]
+                            ],
+                            '#f8e297'
                         )}
                         {new resumeItem(
+                            "bgu",
                             "Plant Lipid Biotechnology Lab at Ben Gurion University",
                             "Research Assistant",
                             [
@@ -92,9 +244,11 @@ function Resume() {
                                 "https://www.vectorlogo.zone/logos/python/python-icon.svg",
                                 "https://raw.githubusercontent.com/simple-icons/simple-icons/master/icons/anaconda.svg",
                                 "https://raw.githubusercontent.com/abranhe/programming-languages-logos/master/src/cpp/cpp.svg"
-                            ]
+                            ],
+                            '#f8e297'
                         )}
                         {new resumeItem(
+                            "vyx",
                             "Variantyx Inc.",
                             "Software Development Intern",
                             [
@@ -104,12 +258,20 @@ function Resume() {
                             ],
                             new Date(2016, 5),
                             new Date(2016, 7),
-                            <Image src="https://www.variantyx.com/wp-content/uploads/2020/05/Variantyx-Logo-2017-white.svg" alt="Variantyx logo"/>,
+                            <Image 
+                                src="https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/ispryndd6bbkizck5dth" 
+                                alt="Variantyx logo"
+                                style={{
+                                    backgroundColor: "white",
+                                    borderRadius: "5px",
+                                    padding: "5px"
+                                }} fluid/>,
                             [
                                 "https://www.vectorlogo.zone/logos/python/python-icon.svg",
                                 "https://www.vectorlogo.zone/logos/salesforce/salesforce-ar21.svg",
                                 "https://www.vectorlogo.zone/logos/github/github-icon.svg"
-                            ]
+                            ],
+                            '#f8e297'
                         )}
                     </ul>
                 </Col>
--- src/components/resume/resumeItem.js
+++ src/components/resume/resumeItem.js
@@ -14,38 +14,39 @@ const calc = (x, y, title) => {
 } 
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
-function resumeItem(title, position, bullets, startDate, endDate, logo, tools) {
+function resumeItem(itemId, title, position, bullets, startDate, endDate, logo, tools, itemColor) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [props, set] = useSpring(() => ({ xys: [0,0,1], config: { mass: 5, tension: 350, friction: 40} }));
     return (
         <li className="list-unstyled padding-15">
             <animated.div 
-                id={title}
+                id={itemId}
                 className="card padding-15"
-                onMouseMove={({clientX: x, clientY: y}) => set({ xys: calc(x, y, title)})}
-                onMouseLeave={() => set({ xys: [0,0,1]})}
+                onMouseMove={({clientX: x, clientY: y}) => set({ xys: calc(x, y, itemId)})}
+                onMouseLeave={() => set({ xys: [0,0,1] })}
                 style={{
-                    backgroundColor: '#f8e297',
+                    backgroundColor: itemColor,
                     transform: props.xys.interpolate(trans)
                 }}>
                     <Container>
                         <Row xs>
-                            <Col md={3}>{logo}</Col>
-                            <Col md={5}>
-                                <Row><h3 className="title">{title}</h3></Row>
-                                <Row><h5>{position}</h5></Row>
+                            <Col md={3} className="vertically-center-contents">{logo}</Col>
+                            <Col fluid>
+                                <Col><h3 className="title">{title}</h3></Col>
+                                <Col>
+                                    <Col md="auto"><h5>{position}</h5></Col>
+                                    <Col md="auto"><h5>
+                                        {new Intl.DateTimeFormat("en-GB", {
+                                            year: "numeric",
+                                            month: "short"
+                                        }).format(startDate)} - 
+                                        {new Intl.DateTimeFormat("en-GB", {
+                                            year: "numeric",
+                                            month: "short"
+                                        }).format(endDate)}
+                                    </h5></Col>
+                                </Col>
                             </Col>
-                            <Col />
-                            <Col md="auto"><h5>
-                                {new Intl.DateTimeFormat("en-GB", {
-                                    year: "numeric",
-                                    month: "short"
-                                }).format(startDate)} - 
-                                {new Intl.DateTimeFormat("en-GB", {
-                                    year: "numeric",
-                                    month: "short"
-                                }).format(endDate)}
-                            </h5></Col>
                         </Row>
                         <br/>
                         <Row md="auto">
@@ -53,8 +54,8 @@ function resumeItem(title, position, bullets, startDate, endDate, logo, tools) {
                         </Row>
                         <Row fluid>
                             <Col />
-                            <Row md="auto">
-                            {tools.map((t) => <div style={{margin: 10+'px'}}><Image src={t} height="50"/></div>)}
+                            <Row md="auto" className="padding-15">
+                            {tools.map((t) => <div style={{padding: 10+'px'}}><Image src={t} height="50"/></div>)}
                             </Row>
                             <Col />
                         </Row>
