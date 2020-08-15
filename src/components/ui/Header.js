585b1a5d62253e8feb0a3afa9281986c3e2cb611
Roy <jackman.roy@gmail.com>
2020-08-14 22:21:21 -0400

    Added name to header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -11,6 +11,9 @@ const Header = () => {
                     <Col sm={1}>
                         <a href="/"><Image src={logo} alt="logo" height="75"/></a>
                     </Col>
+                    <Col className="vertically-center-contents">
+                        <a href="/" className="unstyled-link"><h1>Roy Jackman</h1></a>
+                    </Col>
                     <Col />
                     <Col md="auto" className="center-text">
                         <Button block href="/" variant="outline-dark">Home</Button>
