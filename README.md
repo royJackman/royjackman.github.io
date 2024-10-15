69c6c3f542e3468ee562106d3ee1487571720426
Roy Jackman <royjackman@heuritrade.com>
2024-10-15 13:00:21 -0400

    Merge vue/dist into gh-pages

--- README.md
+++
@@ -1,3 +0,0 @@
-# Roy Jackman Personal Website
-
-Why are you reading this? The whole point of this repo is to make the website. Go to the website.
--- src/components/icons/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as IconAngular } from './IconAngular.vue'
-export { default as IconCommunity } from './IconCommunity.vue'
-export { default as IconDocumentation } from './IconDocumentation.vue'
-export { default as IconEcosystem } from './IconEcosystem.vue'
-export { default as IconSupport } from './IconSupport.vue'
-export { default as IconTooling } from './IconTooling.vue'
